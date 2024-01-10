5e0664f30b4e7c56532fb0a9a988922034a5b1d0
bogdan <bogdanristic95@gmail.com>
2024-01-10 10:42:36 +0100

    disable bold styles for rose-pine

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,11 +2,14 @@ require("rose-pine").setup({
     variant = "auto",
     disable_background = true,
     dark_variant = "moon",
+    styles = {
+        bold = false,
+    }
 })
 
 function ColorMyPencils(color)
     color = color or "rose-pine"
-    vim.cmd.colorscheme("rose-pine")
+    vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
