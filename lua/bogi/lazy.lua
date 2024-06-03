2cf76b48d849f6ea3e593dc90ab670a6f7a13f12
scbogdan <bogdan.ristic@smartcat.io>
2024-06-03 15:12:10 +0200

    add copilot chat

--- lua/bogi/lazy.lua
+++ lua/bogi/lazy.lua
@@ -73,5 +73,18 @@ require("lazy").setup({
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
-    }
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+        },
+        opts = {
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+    },
 })
